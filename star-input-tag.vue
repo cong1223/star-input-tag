d3ce73d18f4752894b833606709a13e2f8650f8a
Jammy Yan <64881367+YanJie-001@users.noreply.github.com>
2021-11-23 16:12:35 +0800

    max限制

--- star-input-tag.vue
+++ star-input-tag.vue
@@ -8,152 +8,176 @@ created(){   字符串转数组，定义在父组件
 },
 -->
 <template>
-	<div>
-		<el-tag
-            v-for="(tag,index) in dynamicTags"
-            :key="index" closable
-            :disable-transitions="false"
-            @click="editTag(tag,index)"
-            @close="handleClose(tag)">
-			<span v-if="index!=num">{{tag}}</span>
-			<input
-                class="custom_input"
-                type="text" v-model="words"
-                v-if="index==num"
-                ref="editInput"
-                @keyup.enter="$event.target.blur()"
-                @blur="handleInput(tag,index)">
-		</el-tag>
-		<el-input
-            class="input-new-tag"
-            v-if="inputVisible"
-            v-model="inputValue"
-            ref="saveTagInput"
-            size="small"
-            @keyup.enter.native="handleInputConfirm"
-            @blur="handleInputConfirm">
+  <div class="input-tag">
+    <el-tag
+      v-for="(tag, index) in dynamicTags"
+      :key="tag"
+      closable
+      :disable-transitions="false"
+      @click="editTag(tag, index)"
+      @close="handleClose(tag)"
+    >
+      <span v-if="index != num">{{ tag }}</span>
+      <input
+        class="custom_input"
+        type="text"
+        v-model.trim="words"
+        v-if="index == num"
+        ref="editInput"
+        @keyup.enter="$event.target.blur()"
+        @blur="handleInput(tag, index)"
+      />
+    </el-tag>
+    <el-input
+      class="input-new-tag"
+      v-if="inputVisible"
+      v-model="inputValue"
+      ref="saveTagInput"
+      size="small"
+      @keyup.enter.native="handleInputConfirm"
+      @blur="handleInputConfirm"
+    >
 		</el-input>
-		<el-button
-            v-else
-            class="button-new-tag"
-            size="small"
-            @click="showInput">{{theme}}</el-button>
-	</div>
+    <div class="add-icon" :class="tagList.length>0 || inputVisible ?'ml10':''" v-if="tagList.length < max ">
+      <i class="el-icon-circle-plus-outline" @click="showInput"></i>
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'star-input-tag',
-		model: {
-			prop: 'tagList',
-			event: 'input'
-		},
-		props: {
-			tagList: {
-				type: Array,
-				default () {
-					return []
-				}
-			},
-			theme: {
-				type: String,
-				default: '+ 新标签'
-			}
-
-		},
-		mounted() {
-			console.log()
-		},
-		data() {
-			return {
-				inputVisible: false,
-				inputValue: '',
-				num: -1,
-				words: ''
-			}
-		},
-		computed: {
-			dynamicTags: {
-				get() {
-					return this.tagList;
-				},
-				set(tagList) {
-					this.$emit('input', tagList);
-				}
-			}
-		},
-		methods: {
-			// 数组去重
-			unique(arr) {
-				let x = new Set(arr);
-				return [...x];
-			},
-
-			handleClose(tag) {
-				this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
-			},
-
-			showInput() {
-				this.inputVisible = true;
-				this.$nextTick(_ => {
-					this.$refs.saveTagInput.$refs.input.focus();
-				});
-			},
-
-			handleInputConfirm() {
-				let inputValue = this.inputValue;
-				if (inputValue) {
-					this.dynamicTags.push(inputValue);
-				}
-				this.dynamicTags = this.unique(this.dynamicTags);
-				this.inputVisible = false;
-				this.inputValue = '';
-			},
-			editTag(tag, index) {
-				this.num = index;
-				this.$nextTick(_ => {
-					this.$refs.editInput[0].focus();
-				});
-				this.words = tag;
-			},
-			handleInput(tag, index) {
-				let words = this.words;
-				if (words) {
-					this.dynamicTags[index] = words;
-				}
-				this.dynamicTags = this.unique(this.dynamicTags);
-				this.words = '';
-				this.num = -1;
-			}
-		}
-	}
+export default {
+  name: "input-tag",
+  model: {
+    prop: "tagList",
+    event: "input",
+  },
+  props: {
+    tagList: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+    theme: {
+      type: String,
+      default: "+ 新标签",
+    },
+    max:{
+      type:Number,
+      default:1000
+    }
+  },
+  data() {
+    return {
+      inputVisible: false,
+      inputValue: "",
+      num: -1,
+      words: "",
+    };
+  },
+  computed: {
+    dynamicTags: {
+      get() {
+        return this.tagList;
+      },
+      set(tagList) {
+        this.$emit("input", tagList);
+      },
+    },
+  },
+  methods: {
+    // 数组去重
+    unique(arr) {
+      let x = new Set(arr);
+      return [...x];
+    },
+    handleClose(tag) {
+      this.dynamicTags.splice(this.dynamicTags.indexOf(tag), 1);
+    },
+    showInput() {
+      this.inputVisible = true;
+      this.$nextTick(_ => {
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
+    },
+    handleInputConfirm() {
+      let inputValue = this.inputValue;
+      if (inputValue) {
+        this.dynamicTags.push(inputValue);
+      }
+      this.dynamicTags = this.unique(this.dynamicTags);
+      this.inputVisible = false;
+      this.inputValue = '';
+    },
+    editTag(tag, index) {
+      this.num = index;
+      this.$nextTick((_) => {
+        this.$refs.editInput[0].focus();
+      });
+      this.words = tag;
+    },
+    handleInput(tag, index) {
+      let words = this.words;
+      if (words) {
+        this.dynamicTags[index] = words;
+      }
+      this.dynamicTags = this.unique(this.dynamicTags);
+      this.words = "";
+      this.num = -1;
+    }
+  }
+};
 </script>
-<style scoped lang="scss">
-	.el-tag+.el-tag {
-		margin-left: 10px;
-	}
+<style scoped lang="less">
+.input-tag {
+  display: flex;
+  align-items: center;
+  height: 40px;
+}
 
-	.button-new-tag {
-		margin-left: 10px;
-		height: 32px;
-		line-height: 30px;
-		padding-top: 0;
-		padding-bottom: 0;
-	}
+.add-icon {
+  width: 40px;
+  height: 40px;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
 
-	.input-new-tag {
-		width: 90px;
-		margin-left: 10px;
-		vertical-align: bottom;
-	}
+  i {
+    cursor: pointer;
+  }
+}
+.el-tag {
+  // margin-top: 4px ;
+}
+.el-tag + .el-tag {
+  margin-left: 10px;
+}
 
-	.custom_input {
-		width: 80px;
-		height: 16px;
-		outline: none;
-		border: transparent;
-		background-color: transparent;
-		font-size: 12px;
-		color: #B59059;
-	}
+.button-new-tag {
+  margin-left: 10px;
+  height: 32px;
+  line-height: 30px;
+  padding-top: 0;
+  padding-bottom: 0;
+}
+.input-new-tag {
+  width: 90px;
+  margin-left: 10px;
+  vertical-align: bottom;
+
+  &:first-child {
+    margin-left: 0;
+  }
+}
+
+
+.custom_input {
+  width: 80px;
+  height: 16px;
+  outline: none;
+  border: transparent;
+  background-color: transparent;
+  font-size: 12px;
+  color: #409eff;
+}
 </style>
